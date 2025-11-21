44eee82b062258d43dff081b8646c95044f7203d
Michał Lisowski <lisu@riseup.net>
2025-11-21 18:05:43 +0100

    firefox: disable hard OCSP fail

--- dot_mozilla/firefox/main/user-overrides.js
+++ dot_mozilla/firefox/main/user-overrides.js
@@ -1,3 +1,6 @@
-// Michal — keep cookies and sessions after shutdown
+// Michal - keep cookies and sessions after shutdown
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+
+// Michal - Disable the OCSP hard-fail
+user_pref("security.OCSP.require", false);
